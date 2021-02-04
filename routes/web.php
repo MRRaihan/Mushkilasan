27f9fe250560b72d7e69201801302617e3d35013
Your Name <you@example.com>
2021-02-04 18:17:50 +0600

    Frontend Add in this Project

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', 'HomeController@index')->name('home');
+
 
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin'], function (){
     Route::get('dashboard', 'DashboardController@dashboard')->name('admin.dashboard');
